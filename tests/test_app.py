e0a81713a60d34b7a63ab2e590de177a6adcf480
unknown <hasikaru31@gmail.com>
2025-12-18 15:38:21 +0530

    Practical_Exam

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,4 +4,4 @@ def test_home_status_code():
     client = app.test_client()
     resp = client.get("/")
     assert resp.status_code == 200
-    assert b"Hello! This is a simple Flask app" in resp.data
+    assert b"Hello! This is the python" in resp.data
